30f350e5475dcf322d845b44e23515a740144a93
grood45 <grood45@gmail.com>
2026-04-09 07:50:50 +0530

    🚀 Sport Events Optimization: Increased refresh rate to 2-minutes

--- cron/sportEvents.cron.js
+++ cron/sportEvents.cron.js
@@ -1,8 +1,8 @@
 const cron = require('node-cron');
 const { refreshAllSportEvents } = require('../services/sportEvents.service');
 
-// Refresh specific sport events every 5 minutes
-cron.schedule('*/5 * * * *', async () => {
+// Refresh specific sport events every 2 minutes
+cron.schedule('*/2 * * * *', async () => {
     console.log('⏰ Running Sport Specific Events (Cricket/Soccer/Tennis) refresh cron...');
     await refreshAllSportEvents();
 });
